3e7e6a9f6b502796bd1e02434354b744d181b160
h0ke <m@h0ke.com>
2021-12-17 06:17:45 -0500

    part 1 day 16 2021

--- 2021/16/solve1.py
+++ 2021/16/solve1.py
@@ -3,7 +3,10 @@ from ast import literal_eval
 f = open("input_test.txt", "r")
 # f = open("input.txt", "r")
 
+version_cnt = 0
+
 def parse_literal(bin_input):
+    # print('in parse_literal')
     output = ''
     chunks = [bin_input[i:i+5] for i in range(0, len(bin_input), 5)]
     for c in chunks:
@@ -14,16 +17,59 @@ def parse_literal(bin_input):
     return output
 
 def parse_packet(bin_input):
-    print(bin_input)
+    # print(bin_input)
     v = int(bin_input[0:3],2)
-    print(v)
+    print('version',v)
+    global version_cnt
+    version_cnt += v
     tid = int(bin_input[3:6],2)
-    print(tid)
+    print('tid', tid)
 
+    result = ''
     if tid == 4:
         result = parse_literal(bin_input[6:])
+    else:
+        ltid = bin_input[6]
+        # print('ltid', ltid)
+        if ltid == '0':
+            # print('BY LENGTH')
+            # print('bin length', bin_input[7:22])
+            length = int(bin_input[7:22],2)
+            # print('dec length', length)
+            parsed_bits = 0
+            while parsed_bits < length:
+                idx = 22 + parsed_bits
+                p = parse_packet(bin_input[idx:])
+                if p[1] == 4:
+                    print('the p literal', p)
+                    bit_groups = len(p[2])/4
+                    parsed_bits += (6 + (bit_groups*5))
+                    # print('parsed_bits', parsed_bits)
+                else:
+                    # print('the p operator', p)
+                    parsed_bits += len(p[2])
 
-    return result
+            result = bin_input[0:(22+parsed_bits)]
+        elif ltid == '1':
+            # print('BY NUM')
+            num_sp = int(bin_input[7:18],2)
+            # print('num_sp', num_sp)
+            parsed_bits = 0
+            for n in range(num_sp):
+                idx = 18 + parsed_bits
+                p = parse_packet(bin_input[idx:])
+                if p[1] == 4:
+                    print('the p literal', p)
+                    bit_groups = len(p[2])/4
+                    parsed_bits += (6 + (bit_groups*5))
+                    # print('parsed_bits', parsed_bits)
+                else:
+                    # print('the p operator', p)
+                    parsed_bits += len(p[2])
+
+            result = bin_input[0:(18+parsed_bits)]
+
+    return (v, tid, result)
 
 def parse_2_bin(input):
     d = literal_eval('0x'+input)
@@ -32,4 +78,6 @@ def parse_2_bin(input):
 
 input = [line.strip() for line in f]
 
-print(parse_packet(parse_2_bin(input[0])))
+parse_packet(parse_2_bin(input[0]))
+
+print(version_cnt)
--- 2021/16/solve2.py
+++ 2021/16/solve2.py
@@ -1,54 +1,83 @@
-# f = open("input_test.txt", "r")
-f = open("input.txt", "r")
+from ast import literal_eval
 
-def count_chars(pairs, final_char):
-    char_count = {}
+f = open("input_test.txt", "r")
+# f = open("input.txt", "r")
 
-    for p in pairs:
-        if p[0] in char_count:
-            char_count[p[0]] += pairs[p]
-        else:
-            char_count[p[0]] = pairs[p]
+version_cnt = 0
 
-    char_count[final_char] += 1
-    return char_count
+def parse_literal(bin_input):
+    # print('in parse_literal')
+    output = ''
+    chunks = [bin_input[i:i+5] for i in range(0, len(bin_input), 5)]
+    for c in chunks:
+        output += c[1:]
+        if c[0] == '0':
+            break;
 
-def do_step(pairs, rules):
-    new_pairs = {}
-    for p in pairs:
-        parts = [p[0]+rules[p], rules[p]+p[1]]
-        for pa in parts:
-            if pa in new_pairs:
-                new_pairs[pa] += pairs[p]
-            else:
-                new_pairs[pa] = pairs[p]
+    return output
 
-    return new_pairs
+def parse_packet(bin_input):
+    # print(bin_input)
+    v = int(bin_input[0:3],2)
+    print('version',v)
+    global version_cnt
+    version_cnt += v
+    tid = int(bin_input[3:6],2)
+    print('tid', tid)
 
-def parse_rules(rules):
-    parsed = {}
-    for r in rules:
-        pair, l = r.split(' -> ')
-        parsed[pair] = l
+    result = ''
+    if tid == 4:
+        result = parse_literal(bin_input[6:])
+    else:
+        ltid = bin_input[6]
+        # print('ltid', ltid)
+        if ltid == '0':
+            # print('BY LENGTH')
+            # print('bin length', bin_input[7:22])
+            length = int(bin_input[7:22],2)
+            # print('dec length', length)
+            parsed_bits = 0
+            while parsed_bits < length:
+                idx = 22 + parsed_bits
+                p = parse_packet(bin_input[idx:])
+                if p[1] == 4:
+                    print('the p literal', p)
+                    bit_groups = len(p[2])/4
+                    parsed_bits += (6 + (bit_groups*5))
+                    # print('parsed_bits', parsed_bits)
+                else:
+                    # print('the p operator', p)
+                    parsed_bits += len(p[2])
 
-    return parsed
+            result = bin_input[0:(22+parsed_bits)]
+        elif ltid == '1':
+            # print('BY NUM')
+            num_sp = int(bin_input[7:18],2)
+            # print('num_sp', num_sp)
+            parsed_bits = 0
+            for n in range(num_sp):
+                idx = 18 + parsed_bits
+                p = parse_packet(bin_input[idx:])
+                if p[1] == 4:
+                    print('the p literal', p)
+                    bit_groups = len(p[2])/4
+                    parsed_bits += (6 + (bit_groups*5))
+                    # print('parsed_bits', parsed_bits)
+                else:
+                    # print('the p operator', p)
+                    parsed_bits += len(p[2])
+
+            result = bin_input[0:(18+parsed_bits)]
+
+    return (v, tid, result)
+
+def parse_2_bin(input):
+    d = literal_eval('0x'+input)
+    str_len = 4 * len(input)
+    return format(d, '0'+str(str_len)+'b')
 
 input = [line.strip() for line in f]
 
-template = input[0]
-rules = parse_rules(input[2:])
+parse_packet(parse_2_bin(input[0]))
 
-pairs_list = [''.join(pair) for pair in zip(template[:-1], template[1:])]
-pairs = {}
-for p in pairs_list:
-    if p in pairs:
-        pairs[p] += 1
-    else:
-        pairs[p] = 1
-
-steps = 40
-for s in range(steps):
-    pairs = do_step(pairs, rules)
-
-count = count_chars(pairs, template[-1])
-print(max(count.values()) - min(count.values()))
+print(version_cnt)
