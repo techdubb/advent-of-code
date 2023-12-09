53ba45611ebf4748955c0bd33f3d05a9879f2818
h0ke <m@h0ke.com>
2023-12-09 08:11:04 -0500

    adding day 9

---
+++ 2023/9/solve1.py
@@ -0,0 +1,37 @@
+# f = open("input_test.txt", "r")
+f = open("input.txt", "r")
+
+def check(x):
+    return x and [x[0]]*len(x) == x and ( x[0] == 0)
+
+def breakdown(s):
+    layers = [s]
+    layer = s
+    while not check(layer):
+        layer = [layer[i+1] - layer[i] for i in range(0, len(layer)-1)]
+        layers.append(layer)
+
+    return layers
+
+def get_next_element(b):
+    b.reverse()
+    next = 0
+    for layer in b:
+        next += layer[-1]
+
+    return next
+
+sequences = []
+for line in f:
+    l = [int(x) for x in line.strip().split(' ')]
+    if l:
+        sequences.append(l)
+
+# print(sequences)
+breakdowns = [breakdown(s) for s in sequences]
+
+# print(breakdowns)
+nexts = [get_next_element(b) for b in breakdowns]
+
+print(sum(nexts))
+
